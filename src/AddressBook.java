074afaeafa0ee13d54c63a1ec39c725582efffc9
ymuzi <ymuziel2003@gmail.com>
2024-09-17 15:17:23 -0400

    Commit

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AddressBook {
-    private final List<BuddyInfo> buddies;  // 'final' keyword added, and the list is initialized right away
+    private final List<BuddyInfo> buddies;  // 'final' keyword used as the list is not reassigned
 
     public AddressBook() {
         this.buddies = new ArrayList<>();
@@ -11,25 +11,25 @@ public class AddressBook {
     public void addBuddy(BuddyInfo buddy) {
         if (buddy != null) {
             this.buddies.add(buddy);
+            System.out.println("Added: " + buddy.getName());  // Output for adding a buddy
         }
     }
 
     public void removeBuddy(BuddyInfo buddy) {
-        this.buddies.remove(buddy);
-    }
-
-    public void displayBuddies() {
-        for (BuddyInfo buddy : this.buddies) {
-            System.out.println(buddy.getName());
+        if (this.buddies.contains(buddy)) {
+            this.buddies.remove(buddy);
+            System.out.println("Removed: " + buddy.getName());  // Output for removing a buddy
         }
     }
 
     public static void main(String[] args) {
         AddressBook addressBook = new AddressBook();
         BuddyInfo buddy = new BuddyInfo("Homer Simpson");
+
+        // Adding BuddyInfo to AddressBook
         addressBook.addBuddy(buddy);
-        System.out.println("Address Book:");
-        addressBook.displayBuddies(); // Display all buddies to address the warning
+
+        // Removing BuddyInfo from AddressBook
         addressBook.removeBuddy(buddy);
     }
 }
--- src/BuddyInfo.java
+++ src/BuddyInfo.java
@@ -1,22 +1,16 @@
 public class BuddyInfo {
-    private String name;
+    private final String name;  // 'final' keyword added as the name does not change
 
     public BuddyInfo(String name) {
         this.name = name;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public static void main(String[] args) {
-        BuddyInfo buddy = new BuddyInfo("Homer");
-        System.out.println("Hello " + buddy.getName());
+        BuddyInfo buddy = new BuddyInfo("Homer Simpson");
+        System.out.println("Hello, " + buddy.getName());
     }
 }
-
-
-//What is the name of the default folders and files that appear in the Java project folder? src/ out/ idea/
-//Name three default Java libraries included in a Java project by IntelliJ.  java.lang java.util java.io
-//The path where the file BuddyInfo.class lives /project-directory/out/production/Lab1/BuddyInfo.class
-//The JAR file containing your completed BuddyInfo class which has a main method that instantiates a buddyInfo object and displays “Hello” followed by the name of the buddyInfo object. This JAR file should contain BOTH the source file and the executable.
